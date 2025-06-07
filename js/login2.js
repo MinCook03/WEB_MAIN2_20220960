07be20b7f4844d2f56382008d3e024e3b1499216
admin <sigeng123@naver.com>
2025-06-07 15:31:56 +0900

    12주차

--- js/login2.js
+++ js/login2.js
@@ -316,7 +316,8 @@ function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    init();
+    checkAuth();
+    init_logined();
 });
 
 function session_set() { //세션 저장
@@ -340,5 +341,13 @@ else{
     }
 }
 
+function session_del() {//세션 삭제
+    if (sessionStorage) {
+        sessionStorage.removeItem("Session_Storage_test");
+        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
 
 
